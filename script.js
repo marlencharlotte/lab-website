f961636ce36d25e7cff17dda6f3dc0aae1123501
Marlen Lauffer <93141861+marlencharlotte@users.noreply.github.com>
2026-03-14 12:11:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menuButton = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.site-nav');
 
+if (menuButton && nav) {
+  menuButton.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+}
+
+const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+document.querySelectorAll('.site-nav a').forEach((link) => {
+  const href = link.getAttribute('href');
+  if (href === currentPage) {
+    link.classList.add('active');
+  }
+});
